7fe7312c5460a07b7e0826fea848ebfbbfc761b3
cryptozeny <cryptozeny@gmail.com>
2018-12-09 08:39:40 +0900

    add: yespower sugarchain remove: yespower NULL

--- yespower/yespower.c
+++ yespower/yespower.c
@@ -1,7 +1,13 @@
 #include "yespower.h"
 
 void yespower_hash(const char *input, char *output, uint32_t len) {
-    static const yespower_params_t params = {YESPOWER_1_0, 2048, 32, NULL, 0};
+    static const yespower_params_t params = {
+        .version = YESPOWER_1_0,
+        .N = 2048,
+        .r = 32,
+        .pers = "Sugarchain: Decentralized Cryptocurrency for one-CPU-one-vote",
+        .perslen = 61
+    };
     yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
